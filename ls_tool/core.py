c16edad5c67045abeea571347eafd43e767d5fd8
madhumithaaravelli <madhumithaaravelli@gmail.com>
2024-11-25 10:21:48 -0500

    Refactored entry details to get_entry_details method

--- ls_tool/core.py
+++ ls_tool/core.py
@@ -25,6 +25,21 @@ def get_file_checksum(filepath, algorithm="md5"):
     return hash_func.hexdigest()
 
 
+def get_entry_details(root, entries):
+
+    "Helper function for list_Directory"
+    
+    return [
+                {
+                    "name": entry,
+                    "size": os.path.getsize(os.path.join(root, entry)),
+                    "mtime": os.path.getmtime(os.path.join(root, entry)),
+                    "type": "directory" if os.path.isdir(os.path.join(root, entry)) else "file",
+                    "permissions": get_file_permissions(os.path.join(root, entry))
+                }
+                for entry in entries
+            ]
+
 def list_directory(path='.', show_all=False, recursive=False, sort_by=None):
     """
     Lists the contents of a directory with optional flags.
@@ -39,16 +54,7 @@ def list_directory(path='.', show_all=False, recursive=False, sort_by=None):
     if recursive:
         for root, dirs, files in os.walk(path):
             entries = dirs + files if show_all else [entry for entry in dirs + files if not entry.startswith('.')]
-            entry_details = [
-                {
-                    "name": entry,
-                    "size": os.path.getsize(os.path.join(root, entry)),
-                    "mtime": os.path.getmtime(os.path.join(root, entry)),
-                    "type": "directory" if os.path.isdir(os.path.join(root, entry)) else "file",
-                    "permissions": get_file_permissions(os.path.join(root, entry))
-                }
-                for entry in entries
-            ]
+            entry_details = get_entry_details(path, entries)
             if sort_by == 'size':
                 entry_details.sort(key=lambda x: x["size"])
             elif sort_by == 'mtime':
@@ -58,16 +64,7 @@ def list_directory(path='.', show_all=False, recursive=False, sort_by=None):
         entries = os.listdir(path)
         if not show_all:
             entries = [entry for entry in entries if not entry.startswith('.')]
-        entry_details = [
-            {
-                "name": entry,
-                "size": os.path.getsize(os.path.join(path, entry)),
-                "mtime": os.path.getmtime(os.path.join(path, entry)),
-                "type": "directory" if os.path.isdir(os.path.join(path, entry)) else "file",
-                "permissions": get_file_permissions(os.path.join(path, entry))
-            }
-            for entry in entries
-        ]
+        entry_details = get_entry_details(path, entries)
         if sort_by == 'size':
             entry_details.sort(key=lambda x: x["size"])
         elif sort_by == 'mtime':
